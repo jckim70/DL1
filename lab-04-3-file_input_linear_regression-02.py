2d76a4425f2ce3f5c5cbb61c8c9dfb6797d2a662
jckim70 <33938850+jckim70@users.noreply.github.com>
2017-11-29 19:28:32 +0900

    20171129

--- lab-04-3-file_input_linear_regression-02.py
+++ lab-04-3-file_input_linear_regression-02.py
@@ -3,13 +3,31 @@ import tensorflow as tf
 import numpy as np
 tf.set_random_seed(777)  # for reproducibility
 
-#xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+def MinMaxScaler(data):
+    numerator = data - np.min(data, 0)
+    denominator = np.max(data, 0) - np.min(data, 0)
+    # noise term prevents the zero division
+#    return numerator / (denominator + 1e-7)
+    return numerator / (denominator)
+
+xy = np.loadtxt('kospi01-1000.csv', delimiter=',', dtype=np.float32)
+#xy = np.loadtxt('data-01-test-score02.csv', delimiter=',', dtype=np.float32)
 #x_data = xy[:, 0:-1]
 #y_data = xy[:, [-1]]
-xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+'''
+numerator = xy - np.min(xy, 0)
+denominator = np.max(xy, 0) - np.min(xy, 0)
+result =  numerator / denominator
+
+#print("num", numerator, denominator, result)
+print("num", numerator)
+print("den", denominator)
+print("res",result)
+
+xy = MinMaxScaler(xy)
+'''
 x_data = xy[:, 0:3]
 y_data = xy[:, 3:4]
-
 
 # Make sure the shape and data are OK
 print(x_data.shape, x_data, len(x_data))
@@ -22,15 +40,15 @@ Y = tf.placeholder(tf.float32, shape=[None, 1])
 W = tf.Variable(tf.random_normal([3, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
+
 # Hypothesis
-#hypothesis = tf.matmul(X, W) + b
 hypothesis = tf.matmul(X, W)
 
 # Simplified cost/loss function
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
 train = optimizer.minimize(cost)
 
 # Launch the graph in a session.
@@ -38,21 +56,20 @@ sess = tf.Session()
 # Initializes global variables in the graph.
 sess.run(tf.global_variables_initializer())
 
-for step in range(2001):
+for step in range(1500001):
     cost_val, hy_val, _ = sess.run(
         [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
-
-#    if step % 1000 == 0:
-    print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+    if step % 100000 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
 
 # Ask my score
+...
 print("Your score will be ", sess.run(
-    hypothesis, feed_dict={X: [[100, 70, 101]]}
-    ))
+    hypothesis, feed_dict={X: [[0.33195, 0.3321, 0.3303]]}))
 
-print("Other scores will be ", sess.run(
-    hypothesis, feed_dict={X: [[60, 70, 110]]}
-    ))
+print("Other scores will be ", sess.run(hypothesis,
+                                        feed_dict={X: [[331.95, 332.1, 330.3]]}))
+...
 
 '''
 Your score will be  [[ 181.73277283]]
--- lab-04-3-file_input_linear_regression-f64.py
+++ lab-04-3-file_input_linear_regression-f64.py
@@ -3,34 +3,34 @@ import tensorflow as tf
 import numpy as np
 tf.set_random_seed(777)  # for reproducibility
 
-#xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+#xy = np.loadtxt('kospi01.csv', delimiter=',', dtype=np.float64)
+xy = np.loadtxt('data-01-test-score02.csv', delimiter=',', dtype=np.float64)
 #x_data = xy[:, 0:-1]
 #y_data = xy[:, [-1]]
-xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+
 x_data = xy[:, 0:3]
 y_data = xy[:, 3:4]
-
 
 # Make sure the shape and data are OK
 print(x_data.shape, x_data, len(x_data))
 print(y_data.shape, y_data)
 
 # placeholders for a tensor that will be always fed.
-X = tf.placeholder(tf.float32, shape=[None, 3])
-Y = tf.placeholder(tf.float32, shape=[None, 1])
+X = tf.placeholder(tf.float64, shape=[None, 3])
+Y = tf.placeholder(tf.float64, shape=[None, 1])
 
 W = tf.Variable(tf.random_normal([3, 1]), name='weight')
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
+
 # Hypothesis
-#hypothesis = tf.matmul(X, W) + b
 hypothesis = tf.matmul(X, W)
 
 # Simplified cost/loss function
 cost = tf.reduce_mean(tf.square(hypothesis - Y))
 
 # Minimize
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
 train = optimizer.minimize(cost)
 
 # Launch the graph in a session.
@@ -41,18 +41,15 @@ sess.run(tf.global_variables_initializer())
 for step in range(2001):
     cost_val, hy_val, _ = sess.run(
         [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
-
-#    if step % 1000 == 0:
-    print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+    if step % 10 == 0:
+        print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val, hypothesis)
 
 # Ask my score
 print("Your score will be ", sess.run(
-    hypothesis, feed_dict={X: [[100, 70, 101]]}
-    ))
+    hypothesis, feed_dict={X: [[100, 70, 101]]}))
 
-print("Other scores will be ", sess.run(
-    hypothesis, feed_dict={X: [[60, 70, 110]]}
-    ))
+print("Other scores will be ", sess.run(hypothesis,
+                                        feed_dict={X: [[60, 70, 110], [90, 100, 80]]}))
 
 '''
 Your score will be  [[ 181.73277283]]
